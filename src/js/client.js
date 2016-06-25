f9993adc8208349ccbf44cf208b85d1116c2047c
Jwing28 <jonathan.wing.lee@gmail.com>
2016-06-24 18:55:23 -0700

    [add] add css file and setup working demo of client

--- src/js/client.js
+++ src/js/client.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Main extends React.Component {
-  render() {
+var Main = React.createClass({
+  getInitialState: function() {
+    return {
+      clicks: 0
+    };
+  },
+
+  onCowClick: function(evt) {
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  },
+
+  render: function() {
     return (
       <div>
-        <h1>Testing App 123!</h1>
+        <div>Clicks: {this.state.clicks}</div>
+        <button onClick={this.onCowClick}>Click ME</button>
       </div>
     );
   }
-}
+});
+
 
 const app = document.getElementById('app');
 ReactDOM.render(<Main />, app);
+
